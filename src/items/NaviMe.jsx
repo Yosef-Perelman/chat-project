0daa1acfdad54d0f0ecd0ee20aec15db977971b0
arielhy11 <arielhy11@gmail.com>
2022-04-24 22:12:10 +0300

    added css to message

--- src/items/NaviMe.jsx
+++ src/items/NaviMe.jsx
@@ -1,9 +1,11 @@
+import { Nav } from "react-bootstrap";
 
-export default function NaviMe(props){
-    return 
-    (<div>
-        <p>this is a message component</p>
-        <h2>{props.key}</h2>
-        <p>{props.text}</p>
-    </div>)
+function NaviMe({ name }) {
+    return (
+        <Nav.Item>
+            <Nav.Link eventKey={name}>{name}</Nav.Link>
+        </Nav.Item>
+    );
 }
+
+export default NaviMe;
