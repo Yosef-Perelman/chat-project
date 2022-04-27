0c8ca8feb1d962b1ff450f858da303dc34f911fe
arielhy11 <arielhy11@gmail.com>
2022-04-28 01:20:05 +0300

    made sending image work

--- src/items/Message.jsx
+++ src/items/Message.jsx
@@ -1,6 +1,13 @@
 import "./message.css"
-function Message({ text }) {
-    return (
+function Message({ text, type, imgSrc, me_or_friend }) {
+    if (type == "image") {
+        return (
+            <div className="message">
+                <img src={imgSrc} />
+            </div>
+        );
+    }
+    else return (
         <div className="message">
             <p className="messageText">{text}</p>
         </div>
