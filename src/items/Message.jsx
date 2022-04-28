2827728c3e3e3d99fc968fc29292d8e7cef43140
arielhy11 <arielhy11@gmail.com>
2022-04-28 14:08:08 +0300

    added time and user\friend distinguish

--- src/items/Message.jsx
+++ src/items/Message.jsx
@@ -1,16 +1,46 @@
 import "./message.css"
 function Message({ text, type, imgSrc, me_or_friend }) {
-    if (type == "image") {
+    var time = new Date();
+    //IMAGES!
+    if (type == "image" && me_or_friend == "me") {
         return (
-            <div className="message">
-                <img src={imgSrc} />
+            <div className="mine">
+                <div class="imgblock">
+                    <img src={imgSrc} />
+                    <div class="smallblackarea">{time.getHours() + ":" + time.getMinutes()}</div>
+                </div>
+            </div>
+
+        );
+    }
+    else if (type == "image" && me_or_friend == "friend") {
+        return (
+            <div className="friend">
+                <div class="imgblock">
+                    <img src={imgSrc} />
+                    <div className="smallblackarea">{time.getHours() + ":" + time.getMinutes()}</div>
+                </div>
             </div>
         );
     }
-    else return (
-        <div className="message">
-            <p className="messageText">{text}</p>
-        </div>
-    );
+
+    //MESSAGES!
+    else if (type == "text" && me_or_friend == "me") {
+        return (
+            <div className="mine">
+                <div class="imgblock">
+                    <p className="messageText">{text}</p>
+                    <div class="smallblackarea-text">{time.getHours() + ":" + time.getMinutes()}</div>
+                </div>
+            </div>
+        );
+    }
+    else if (type == "text" && me_or_friend == "friend") {
+        return (
+            <div className="friend">
+                <p className="messageText">{text}</p>
+            </div>
+        );
+    }
 }
 export default Message;
